eb14259f1673496a509e5b5afa360eadcb064cd2
bcrich <lastra10@hotmail.com>
2022-08-15 22:47:10 -0500

    third AppDelegate comment from origin/master

--- Testing-git/AppDelegate.swift
+++ Testing-git/AppDelegate.swift
@@ -13,6 +13,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //AppDelegate comment from origin/master
     
     //second AppDelegate comment from origin/master
+    
+    //third AppDelegate comment from origin/master
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
