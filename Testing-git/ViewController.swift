65dc0f26263f5607fc63581cdb6f3df530dbc899
bcrich80 <bcrich80@gmail.com>
2022-08-16 00:55:13 -0500

    uilabel extension

--- Testing-git/ViewController.swift
+++ Testing-git/ViewController.swift
@@ -9,8 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //var vertivalConstraint: CGFloat = 20
+    
+    var correctVerticalConstraint: CGFloat = 30
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //var edgesForlabel = LabelClass()
+        
+        var thelabel: UILabel!
+        thelabel.margins()
+    }
+    
+}
+
+extension UILabel {
+    
+    func margins() {
+        
     }
 }
+
+//class LabelClass {
+//
+//    func edges() {
+//
+//    }
+//}
