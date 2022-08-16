3cf7d156d7cd64159d01e7f478f729030c10f4cb
bcrich80 <bcrich80@gmail.com>
2022-08-15 22:36:39 -0500

    first commit

---
+++ Testing-git/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Testing-git
+//
+//  Created by bcrich on 5/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
